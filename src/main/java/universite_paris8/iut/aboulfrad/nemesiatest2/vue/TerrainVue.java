03b598ecabadddc8e8b1271cf8fe83e848855d9c
Thileepan <Thileepanedvin@gmail.com>
2025-05-22 17:24:47 +0200

    Modification du Terrain ( Style )

--- src/main/java/universite_paris8/iut/aboulfrad/nemesiatest2/vue/TerrainVue.java
+++ src/main/java/universite_paris8/iut/aboulfrad/nemesiatest2/vue/TerrainVue.java
@@ -5,7 +5,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 import universite_paris8.iut.aboulfrad.nemesiatest2.model.Terrain;
 
-public class  TerrainVue {
+public class TerrainVue {
 
     private Terrain terrain;
     private TilePane tilePane;
@@ -17,27 +17,39 @@ public class  TerrainVue {
     }
 
     public void afficherTerrain () {
-        Image imagedirt = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/dirt.png").toExternalForm());
-        Image imagegrass = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/grass.png").toExternalForm());
-        Image imagesky = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/sky.png").toExternalForm());
+        Image sky = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/sky_dark.png").toExternalForm());
+        Image grass = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/grass_dark.png").toExternalForm());
+        Image dirt = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/dirt_dark.png").toExternalForm());
+        Image imageBlack = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/noir.png").toExternalForm());
+        Image imageWhiteLight = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/blanc.png").toExternalForm());
+        Image imageWhiteDark = new Image(getClass().getResource("/universite_paris8/iut/aboulfrad/nemesiatest2/image/gris.png").toExternalForm());
 
-        tilePane.setPrefSize(32 * terrain.hauteur(),32 * terrain.largeur());
+        tilePane.setPrefSize(32 * terrain.hauteur(), 32 * terrain.largeur());
 
         for (int i = 0; i < terrain.largeur(); i++) {
             for (int j = 0; j < terrain.hauteur(); j++) {
                 ImageView iv = new ImageView();
-                if (terrain.typeTuile(i,j) == 1) {
-                    iv.setImage(imagesky);
-                } else if (terrain.typeTuile(i,j) == 2) {
-                    iv.setImage(imagegrass);
-                } else if (terrain.typeTuile(i,j) == 3) {
-                    iv.setImage(imagedirt);
+                int tuile = terrain.typeTuile(i, j);
+
+                if (tuile == 1) {
+                    iv.setImage(sky);
+                } else if (tuile == 2) {
+                    iv.setImage(grass);
+                } else if (tuile == 3) {
+                    iv.setImage(dirt);
+                } else if (tuile == 4) {
+                    iv.setImage(imageBlack);
+                } else if (tuile == 5) {
+                    iv.setImage(imageWhiteLight);
+                } else if (tuile == 6) {
+                    iv.setImage(imageWhiteDark);
                 }
 
-                tilePane.getChildren().add(iv);
-                iv.setFitHeight(32);
                 iv.setFitWidth(32);
+                iv.setFitHeight(32);
+                tilePane.getChildren().add(iv);
             }
         }
+
     }
 }
